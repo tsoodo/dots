0eb2186c0eba031b7140361f5524b2ff81a77827
Ian <me@ianirizarry.com>
2025-05-15 23:07:29 -0400

    idk

--- nvim/lua/ii/plugins/coding.lua
+++ nvim/lua/ii/plugins/coding.lua
@@ -2,10 +2,9 @@ return {
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
-    version = false, -- Never set this value to "*"! Never!
+    version = false,
     opts = {
-      -- add any opts here
-      -- for example
+      theme = "auto",
       provider = "openai",
       openai = {
         endpoint = "https://api.openai.com/v1",
@@ -13,7 +12,7 @@ return {
         timeout = 30000,              -- Timeout in milliseconds, increase this for reasoning models
         temperature = 0,
         max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-        reasoning_effort = "medium",  -- low|medium|high, only used for reasoning models
+        reasoning_effort = "low",     -- low|medium|high, only used for reasoning models
       },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
@@ -30,24 +29,6 @@ return {
       "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
       "ibhagwan/fzf-lua",              -- for file_selector provider fzf
       "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua",        -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
       {
         -- Make sure to set this up properly if you have lazy=true
         'MeanderingProgrammer/render-markdown.nvim',
--- nvim/lua/ii/plugins/telescope.lua
+++ nvim/lua/ii/plugins/telescope.lua
@@ -1,32 +1,33 @@
 -- lua/ii/plugins/telescope.lua
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = { 'nvim-telescope/telescope-ui-select.nvim', "nvim-lua/plenary.nvim", { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' } },
-    config = function()
-        require('telescope').setup({
-            extensions = {
-                fzf = {
-                    fuzzy = true,
-                    override_generic_sorter = true,
-                    override_file_sorter = true,
-                    case_mode = "smart_case",
-                }
-            }
-        })
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', ';f', builtin.find_files, { silent = true })
-        vim.keymap.set("n", ";g", builtin.live_grep, { silent = true })
-        vim.keymap.set("n", ";r", ":Telescope oldfiles<CR>", { silent = true })
-        vim.keymap.set('n', ';h', builtin.help_tags, { desc = 'Telescope help tags' })
-        -- vim.keymap.set('n', '<leader>c', builtin.commands, { desc = 'Telescope help tags' })
-        vim.keymap.set('n', '<leader>co', builtin.colorscheme, { desc = 'Telescope help tags' })
-        vim.keymap.set('n', '<leader>lt', builtin.treesitter, { desc = 'List functions' })
-        vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, { desc = 'List functions' })
-        vim.keymap.set('n', '<leader>lq', '<cmd>Telescope diagnostics<CR>', { desc = 'List functions' })
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.5",
+  dependencies = { 'nvim-telescope/telescope-ui-select.nvim', "nvim-lua/plenary.nvim", { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' } },
+  config = function()
+    require('telescope').setup({
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        }
+      }
+    })
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', ';f', builtin.find_files, { silent = true })
+    vim.keymap.set("n", ";g", builtin.live_grep, { silent = true })
+    vim.keymap.set("n", ";r", ":Telescope oldfiles<CR>", { silent = true })
+    vim.keymap.set('n', ';h', builtin.help_tags, { desc = 'Telescope help tags' })
+    vim.keymap.set('n', ';;', builtin.resume, { desc = 'Telescope resume' })
+    -- vim.keymap.set('n', '<leader>c', builtin.commands, { desc = 'Telescope help tags' })
+    vim.keymap.set('n', '<leader>co', builtin.colorscheme, { desc = 'Telescope help tags' })
+    vim.keymap.set('n', '<leader>lt', builtin.treesitter, { desc = 'List functions' })
+    vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, { desc = 'List functions' })
+    vim.keymap.set('n', '<leader>lq', '<cmd>Telescope diagnostics<CR>', { desc = 'List functions' })
 
-        require('telescope').load_extension('fzf')
-        require("telescope").load_extension("ui-select")
-    end
+    require('telescope').load_extension('fzf')
+    require("telescope").load_extension("ui-select")
+  end
 
 }
