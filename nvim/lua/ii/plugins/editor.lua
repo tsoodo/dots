012b80ea69b37d95e635a26b840fc75478bade93
ii <me@ianirizarry.com>
2025-05-17 13:10:53 -0400

    idek

--- nvim/lua/ii/plugins/editor.lua
+++ nvim/lua/ii/plugins/editor.lua
@@ -1,5 +1,12 @@
 return {
   {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+  },
+  {
     "lewis6991/gitsigns.nvim",
     opts = {
       signs = {
--- nvim/lua/ii/plugins/treesitter.lua
+++ nvim/lua/ii/plugins/treesitter.lua
@@ -1,52 +1,82 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "vimdoc",
-        "javascript",
-        "typescript",
-        "c",
-        "lua",
-        "rust",
-        "jsdoc",
-        "bash",
-        "typst",
-        "svelte",
-        "python",
-        "glsl",
-      },
-      sync_install = false,
-      auto_install = true,
-      indent = {
-        enable = true,
-      },
-      highlight = {
-        enable = true,
-        disable = function(lang, buf)
-          if lang == "html" then
-            print("disabled treesitter for html")
-            return true
-          end
-          local max_filesize = 100 * 1024 -- 100 KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            vim.notify(
-              "File larger than 100KB treesitter disabled for performance",
-              vim.log.levels.WARN,
-              { title = "Treesitter" }
-            )
-            return true
-          end
-        end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "vimdoc",
+          "javascript",
+          "typescript",
+          "c",
+          "lua",
+          "rust",
+          "jsdoc",
+          "bash",
+          "typst",
+          "svelte",
+          "python",
+          "glsl",
+        },
+        sync_install = false,
+        auto_install = true,
+        indent = {
+          enable = true,
+        },
+        highlight = {
+          enable = true,
+          disable = function(lang, buf)
+            if lang == "html" then
+              print("disabled treesitter for html")
+              return true
+            end
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              vim.notify(
+                "File larger than 100KB treesitter disabled for performance",
+                vim.log.levels.WARN,
+                { title = "Treesitter" }
+              )
+              return true
+            end
+          end,
 
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        -- additional_vim_regex_highlighting = { "markdown" },
-      },
-    })
-  end,
+          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+          -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
+          -- Using this option may slow down your editor, and you may see some duplicate highlights.
+          -- Instead of true it can also be a list of languages
+          -- additional_vim_regex_highlighting = { "markdown" },
+        },
+      })
+    end,
+
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    event = "VeryLazy",
+    enabled = true,
+    config = function()
+      -- When in diff mode, we want to use the default
+      -- vim text objects c & C instead of the treesitter ones.
+      local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
+      local configs = require("nvim-treesitter.configs")
+      for name, fn in pairs(move) do
+        if name:find("goto") == 1 then
+          move[name] = function(q, ...)
+            if vim.wo.diff then
+              local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
+              for key, query in pairs(config or {}) do
+                if q == query and key:find("[%]%[][cC]") then
+                  vim.cmd("normal! " .. key)
+                  return
+                end
+              end
+            end
+            return fn(q, ...)
+          end
+        end
+      end
+    end,
+  }
 }
