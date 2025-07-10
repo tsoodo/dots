21878a579ed7c4ed30f111d96c5f459424593ee3
ii <me@ianirizarry.com>
2025-07-09 23:43:17 -0400

    fix: comment out unneeded plugin configuration for avante.nvim

--- nvim/lua/ii/plugins/coding.lua
+++ nvim/lua/ii/plugins/coding.lua
@@ -1,63 +1,63 @@
 return {
-    -- {
-    --     "yetone/avante.nvim",
-    --     event = "VeryLazy",
-    --     version = false,
-    --     opts = {
-    --         theme = "auto",
-    --         provider = "openai",
-    --         openai = {
-    --             endpoint = "https://api.openai.com/v1",
-    --             model = "o4-mini",            -- your desired model (or use gpt-4o, etc.)
-    --             timeout = 30000,              -- Timeout in milliseconds, increase this for reasoning models
-    --             temperature = 0,
-    --             max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-    --             reasoning_effort = "low",     -- low|medium|high, only used for reasoning models
-    --         },
-    --     },
-    --     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    --     build = "make",
-    --     -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    --     dependencies = {
-    --         "nvim-treesitter/nvim-treesitter",
-    --         "stevearc/dressing.nvim",
-    --         "nvim-lua/plenary.nvim",
-    --         "MunifTanjim/nui.nvim",
-    --         --- The below dependencies are optional,
-    --         "echasnovski/mini.pick",         -- for file_selector provider mini.pick
-    --         "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    --         "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
-    --         "ibhagwan/fzf-lua",              -- for file_selector provider fzf
-    --         "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
-    --         {
-    --             -- Make sure to set this up properly if you have lazy=true
-    --             'MeanderingProgrammer/render-markdown.nvim',
-    --             opts = {
-    --                 file_types = { "Avante" },
-    --             },
-    --             ft = { "Avante" },
-    --         },
-    --     },
-    -- },
-    {
-        "echasnovski/mini.pairs",
-        event = "VeryLazy",
-        opts = {
-            modes = { insert = true, command = true, terminal = false },
-            -- skip autopair when next character is one of these
-            skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
-            -- skip autopair when the cursor is inside these treesitter nodes
-            skip_ts = { "string" },
-            -- skip autopair when next character is closing pair
-            -- and there are more closing pairs than opening pairs
-            skip_unbalanced = true,
-            -- better deal with markdown code blocks
-            markdown = true,
-        },
-    },
-    {
-        "folke/ts-comments.nvim",
-        event = "VeryLazy",
-        opts = {},
-    }
+	-- {
+	--     "yetone/avante.nvim",
+	--     event = "VeryLazy",
+	--     version = false,
+	--     opts = {
+	--         theme = "auto",
+	--         provider = "openai",
+	--         openai = {
+	--             endpoint = "https://api.openai.com/v1",
+	--             model = "o4-mini",            -- your desired model (or use gpt-4o, etc.)
+	--             timeout = 30000,              -- Timeout in milliseconds, increase this for reasoning models
+	--             temperature = 0,
+	--             max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+	--             reasoning_effort = "low",     -- low|medium|high, only used for reasoning models
+	--         },
+	--     },
+	--     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+	--     build = "make",
+	--     -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+	--     dependencies = {
+	--         "nvim-treesitter/nvim-treesitter",
+	--         "stevearc/dressing.nvim",
+	--         "nvim-lua/plenary.nvim",
+	--         "MunifTanjim/nui.nvim",
+	--         --- The below dependencies are optional,
+	--         "echasnovski/mini.pick",         -- for file_selector provider mini.pick
+	--         "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+	--         "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
+	--         "ibhagwan/fzf-lua",              -- for file_selector provider fzf
+	--         "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
+	--         {
+	--             -- Make sure to set this up properly if you have lazy=true
+	--             'MeanderingProgrammer/render-markdown.nvim',
+	--             opts = {
+	--                 file_types = { "Avante" },
+	--             },
+	--             ft = { "Avante" },
+	--         },
+	--     },
+	-- },
+	{
+		"echasnovski/mini.pairs",
+		event = "VeryLazy",
+		opts = {
+			modes = { insert = true, command = true, terminal = false },
+			-- skip autopair when next character is one of these
+			skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+			-- skip autopair when the cursor is inside these treesitter nodes
+			skip_ts = { "string" },
+			-- skip autopair when next character is closing pair
+			-- and there are more closing pairs than opening pairs
+			skip_unbalanced = true,
+			-- better deal with markdown code blocks
+			markdown = true,
+		},
+	},
+	{
+		"folke/ts-comments.nvim",
+		event = "VeryLazy",
+		opts = {},
+	},
 }
