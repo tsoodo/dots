72cc5f135125c887f0f886c39dc8a24333479554
ii <me@ianirizarry.com>
2025-11-09 21:35:29 -0400

    chore: update btop theme, enable theme background, change tmux key bindings and script references

--- scripts/tmux-session-dispensary.sh
+++ scripts/tmux-session-dispensary.sh
@@ -3,13 +3,12 @@
 DIRS=(
     "$HOME/Projects"
     "$HOME/.config"
-    "$HOME"
 )
 
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(fd . "${DIRS[@]}" --type=dir --max-depth=2 --full-path --base-directory $HOME \
+    selected=$(fd . "${DIRS[@]}" --type=dir --max-depth=3 --full-path --base-directory $HOME \
         | sed "s|^$HOME/||" \
         | sk --margin 10% --color="bw")
 
@@ -22,7 +21,7 @@ selected_name=$(basename "$selected" | tr . _)
 
 if ! tmux has-session -t "$selected_name"; then
     tmux new-session -ds "$selected_name" -c "$selected"
-    tmux send-keys -t "$selected_name:1" "nvim ." C-m
+    # tmux send-keys -t "$selected_name:1" "nvim -c 'Telescope find_files'" C-m
     tmux select-window -t "$selected_name:1"
 fi
 
